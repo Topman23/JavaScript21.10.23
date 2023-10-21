bce5e405347cab11e49123b5dc7199ba9f2fd578
Alex <erolex999i@mail.ru>
2023-10-21 18:23:26 +0300

    second

---
+++ js/script.js
@@ -0,0 +1,76 @@
+"use strict";
+
+console.log('arr' + " - object");
+console.log(4 + + "5");
+
+let incr = 10,
+    decr =10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2+2*2 != 8);
+
+const isChecked = true,
+      isClose = false;
+console.log(isChecked || !isClose);
+// const category = ('Toys');
+// console.log(`https://someurl.com/${category}/5/wer`);
+
+// const user = 'Ivan';
+// alert(`Привет, ${user}`);
+
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt('Вам есть 18?', '18');
+// console.log(answer + 5);
+
+// const answers =[];
+
+// // answers[0] = prompt('Как ваше имя?', '');
+// // // answers[1] = prompt('Как ваше фамилия?', '');
+// // // answers[2] = prompt('сколько вам лет?', '');
+
+// console.log(typeof(answers));
+
+// let number = 4.6;
+// console.log(4/0);
+// console.log('string' * 9);
+
+// const persone = 'Alex';
+
+// const bool = false;
+
+// console.log(something);
+
+// let und;
+
+// const obj = {
+//     name: 'John',
+//     age: '25',
+//     isMarried: false
+// };
+// console.log(obj.name);
+
+// let arr = ['plum', 'orange.jpg', 'apple.bmp'];
+// console.log(arr[1]);
+
+// const storeName = "Sneaker Store";
+// const storeDescription = {
+//     budget: 10000,
+//     employees: ['dsf', 'dsfds', 'dsfds'],
+//     products: {
+//         comp: 100,
+//         keyb: 30
+//     },
+//     open: false
+// };
+
